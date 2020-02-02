af612465c4027b027da9882c56e6d80a468e808f
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2020-02-02 12:47:30 +0200

    - refactored the instrumentation test to launch the Registration activity bypassing the newly added splash activity

--- app/src/androidTest/java/com/example/android/dagger/ApplicationTest.kt
+++ app/src/androidTest/java/com/example/android/dagger/ApplicationTest.kt
@@ -22,13 +22,14 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
 import org.junit.Test
 
 class ApplicationTest {
 
     @Test
     fun runApp() {
-        ActivityScenario.launch(MainActivity::class.java)
+        ActivityScenario.launch(RegistrationActivity::class.java)
 
         // Should be in Registration/EnterDetails because the user is not registered
         onView(withText("Register to Dagger World!")).check(matches(isDisplayed()))
